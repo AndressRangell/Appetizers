4d733b89d836f1163e430b0df05504bf100387b7
AndressRangell <andres.rangel555@gmail.com>
2024-05-20 18:50:26 -0500

    add config files

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -29,9 +29,9 @@ struct MockData {
                                            description: "This is the description for my appetizer. It's yummy.",
                                            price: 9.99,
                                            imageURL: "",
-                                           calories: 99,
-                                           protein: 99,
-                                           carbs: 99)
+                                           calories: 100,
+                                           protein: 120,
+                                           carbs: 30)
     
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
     
--- Appetizers/Screens/AppetizerDetail/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerDetail/AppetizerDetailView.swift
@@ -8,13 +8,101 @@
 import SwiftUI
 
 struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 350, height: 263)
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 50) {
+                    VStack {
+                        Text("Calories")
+                            .font(.caption)
+                            .bold()
+                        
+                        Text("\(appetizer.calories)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack {
+                        Text("Carbs")
+                            .font(.caption)
+                            .bold()
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack {
+                        Text("Protein")
+                            .font(.caption)
+                            .bold()
+                        
+                        Text("\(appetizer.protein)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                print("Tapped")
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(12)
+                    .shadow(radius: 40)
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 330, height: 600)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            print("Dismiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.black)
+            }
+        }, alignment: .topTrailing)
     }
 }
 
 struct AppetizerDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        AppetizerDetailView()
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer)
     }
 }
